a18a67bb3ad9704293d980bf5a79dc02a4499f42
Lucas <oliveira.lucas20@gmail.com>
2023-10-29 10:37:30 -0400

    Adds model class and implements click buttons

--- app/src/main/java/com/lucassimao/notaalvo/MainActivity.kt
+++ app/src/main/java/com/lucassimao/notaalvo/MainActivity.kt
@@ -2,10 +2,35 @@ package com.lucassimao.notaalvo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lucassimao.notaalvo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val model = CalculatorModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupButtonListeners()
+    }
+
+    private fun setupButtonListeners() {
+        val numberButtons = listOf(
+            binding.btnOne, binding.btnTwo, binding.btnThree, binding.btnFour,
+            binding.btnFive, binding.btnSix, binding.btnSeven, binding.btnEigth,
+            binding.btnNine, binding.btnZero, binding.btnPoint
+        )
+
+        numberButtons.forEach { button ->
+            button.setOnClickListener {
+                model.addNumber(button.text.toString())
+                updateTextView()
+            }
+        }
+    }
+
+    private fun updateTextView() {
+        binding.txtResult.text = model.getGrade()
     }
 }
