aec254f70a888551c6d663cc0199bcaa8df8bc42
Lucas <oliveira.lucas20@gmail.com>
2023-11-04 01:15:10 -0400

    Implements a new way of deploying

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
+    repositories {
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
+    }
     dependencies {
         classpath("com.google.gms:google-services:4.4.0")
-        classpath ("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
+        classpath("com.github.triplet.gradle:play-publisher:4.0.0-SNAPSHOT")
     }
 }
 
