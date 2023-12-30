e3e3146324a7af02146475503f80c0cbfbd11eab
Lucas de Oliveira Simão <Oliveira.lucas20@gmail.com>
2023-12-30 13:52:32 -0400

    fix: applies suggested by the lint report. Fixed #25
    - adds the last version of targetSDK
    - deletes attribute not used in lower version of API 26
    - updates version of Core and Material dependencies
    - deletes unused resources
    - convert main image to WebP format
    - add inputType to EditText

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.lucassimao.notaalvo"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.lucassimao.notaalvo"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -52,9 +52,9 @@ dependencies {
     implementation("com.google.firebase:firebase-crashlytics-ktx")
     implementation("com.google.firebase:firebase-analytics-ktx")
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
