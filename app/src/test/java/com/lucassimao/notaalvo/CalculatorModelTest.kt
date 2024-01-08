3b067b0aca646456614b6c9610d5a34753560b3c
Lucas de Oliveira Simão <Oliveira.lucas20@gmail.com>
2024-01-08 16:16:51 -0400

    test correction

--- app/src/test/java/com/lucassimao/notaalvo/CalculatorModelTest.kt
+++ app/src/test/java/com/lucassimao/notaalvo/CalculatorModelTest.kt
@@ -108,7 +108,7 @@ class CalculatorModelTest {
         every { calculator.convertUserScoreToDouble() } returns 5.3
         val score = calculator.convertUserScoreToDouble()
 
-        every { calculator.calculateScoreDifference(any()) } returns "4,4"
+        every { calculator.calculateScoreDifference(any()) } returns "4,40"
         val result = calculator.calculateScoreDifference(score)
 
         val expected = kotlin.math.abs(IDEAL_SCORE - (score * MULTIPLIER_VALUE)).formatDoubleWithComma()
