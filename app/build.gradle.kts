5b03d1b8b8b60dfff2d0fe4fb67ba8629d64ed5f
Lucas de Oliveira Simão <Oliveira.lucas20@gmail.com>
2024-03-08 16:26:56 -0400

    Fixes error in the deployment pipeline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.lucassimao.notaalvo"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
+        versionCode = 29
         versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
