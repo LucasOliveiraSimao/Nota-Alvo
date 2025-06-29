52555449149478c2c0d574aec2ad811155557367
Lucas de Oliveira Simão <Oliveira.lucas20@gmail.com>
2025-06-28 23:57:42 -0400

    feat(build): update minimum SDK version to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         applicationId = "com.lucassimao.notaalvo"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 35
         versionCode = 37
         versionName = "3.0.0"
