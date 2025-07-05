56846e8c0ed8c7206c4765b583b6345387d65432
Lucas de Oliveira Simão <Oliveira.lucas20@gmail.com>
2025-07-05 13:52:49 -0400

    refactor(ScoreStatus): move ScoreStatus enum to domain.model package

--- app/src/main/java/com/lucassimao/notaalvo/domain/model/ScoreStatus.kt
+++ app/src/main/java/com/lucassimao/notaalvo/domain/model/ScoreStatus.kt
@@ -1,4 +1,4 @@
-package com.lucassimao.notaalvo.util
+package com.lucassimao.notaalvo.domain.model
 
 enum class ScoreStatus {
     INVALID, APPROVED, NEEDS_FINAL_EXAM, NEEDS_EXAM_FOR_APPROVAL, FAILED
