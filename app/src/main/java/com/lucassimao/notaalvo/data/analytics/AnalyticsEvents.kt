bf78a292c094ccf50a7f28e51256c1f3008a505f
Lucas de Oliveira Simão <Oliveira.lucas20@gmail.com>
2025-07-05 13:38:01 -0400

    feat(analytics): add analytics events constants

---
+++ app/src/main/java/com/lucassimao/notaalvo/data/analytics/AnalyticsEvents.kt
@@ -0,0 +1,18 @@
+package com.lucassimao.notaalvo.data.analytics
+
+object AnalyticsEvents {
+    const val SCREEN_CALCULATOR = "CalculatorScreen"
+    const val SCREEN_ONBOARDING  = "OnboardingScreen "
+    const val BUTTON_CALCULATE = "calculate_score_clicked"
+    const val SHARE_APP = "share_app_clicked"
+    const val APPROVED = "approved_student"
+    const val NEEDS_FINAL_EXAM = "student_needs_to_take_final_exam"
+    const val NEEDS_EXAM_FOR_APPROVAL = "student_passed_but_needs_to_take_the_final_exam"
+    const val FAILED = "failed_student"
+    const val OPEN_RATING = "open_rating"
+    const val NOT_RATING = "not_rating"
+    const val LATER_RATING = "later_rating"
+    const val ON_DONE_PRESSED = "on_done_pressed"
+    const val ON_SKIP_PRESSED = "on_skip_pressed"
+    const val NOTA_INPUT_PLACEHOLDER = "nota_input_placeholder"
+}
